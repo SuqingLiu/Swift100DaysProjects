537c739943c852884fc3665f8b976a9991ecd10d
suqingliu <suqingliu1018@gmail.com>
2025-04-27 17:57:26 -0400

    added projects

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Suqing Liu on 2025-04-25.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
